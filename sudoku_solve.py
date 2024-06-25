26820fb3868fee343036d0ef8c39de23ecf0580d
Dhayanithi T <149855273+DhayanithiT05@users.noreply.github.com>
2024-06-25 21:24:29 +0530

    Add files via upload

---
+++ sudoku_solve.py
@@ -0,0 +1,67 @@
+def is_vaild(board, r, c, num):
+    for x in range(9):
+        if board[r][x] == num:
+            return False
+        
+    for x in range(9):
+        if board[x][c] == num:
+            return False
+        
+    start_row = r - r % 3
+    start_col = c - c % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def sol_sudoku(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    r, c = empty
+    
+    for num in range(1, 10):
+        if is_vaild(board, r, c, num):
+            board[r][c] = num
+            if sol_sudoku(board):
+                return True
+            board[r][c] = 0
+    return False
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        
+        for j in range(9):
+            if j % 3 == 0 and j  != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+                
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if sol_sudoku(board):
+    print_board(board)
+else:
+    print("No solution exists...")
